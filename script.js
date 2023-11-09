644044673ccb2a23325b327ec09a5fdf5244cebe
Shahzad Ahamad P <149555677+shahzadahamad@users.noreply.github.com>
2023-11-09 21:23:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+  const sidebar = document.querySelector('.menu');
+  sidebar.style.display = 'flex';
+}
+function closeSidebar(){
+  const sidebar = document.querySelector('.menu');
+  sidebar.style.display = 'none ';
+}
